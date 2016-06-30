00f70a4dfe8c3f990ab969a0955b91265649f727
Tony Glover <tony.glover@nottingham.ac.uk>
2016-06-30 23:35:58 +0100

    Further refactoring

--- js/baseApp.js
+++ js/baseApp.js
@@ -4,61 +4,45 @@
 //Common baseline for visualisation app
 
 function BaseApp() {
-    /*
-     this.renderer = null;
-     this.scene = null;
-     this.camera = null;
-     this.controls = null;
-     this.stats = null;
-     this.container = null;
-     this.projector = null;
-     this.objectList = [];
-     this.root = null;
-     this.mouse = { startX:0, startY:0, down:false, endX:0, endY:0};
-     this.pickedObjects = [];
-     this.hoverObjects = [];
-     this.startTime = 0;
-     this.elapsedTime = 0;
-     this.clock = new THREE.Clock();
-     */
+    this.renderer = null;
+    this.scene = null;
+    this.camera = null;
+    this.controls = null;
+    this.stats = null;
+    this.container = null;
+    this.objectList = [];
+    this.root = null;
+    this.mouse = { startX:0, startY:0};
+    this.pickedObjects = [];
+    this.selectedObject = null;
+    this.hoverObjects = [];
+    this.startTime = 0;
+    this.elapsedTime = 0;
+    this.clock = new THREE.Clock();
+    this.clock.start();
+    this.objectsPicked = false;
 }
 
 BaseApp.prototype.init = function(container) {
     this.container = container;
-    console.log("BaseApp container =", container);
     this.createRenderer();
-    console.log("BaseApp renderer =", this.renderer);
     this.createCamera();
-    this.createControls();
-    this.projector = new THREE.Projector();
+    //this.createControls();
     //this.stats = initStats();
     this.statsShowing = false;
-
 };
 
 BaseApp.prototype.createRenderer = function() {
     this.renderer = new THREE.WebGLRenderer( {antialias : true, alpha: true});
-    this.renderer.setClearColor(0x000000, 0);
+    //this.renderer.setClearColor(0x5c5f64, 1.0);
     this.renderer.shadowMapEnabled = true;
-    var isMSIE = /*@cc_on!@*/0;
 
-    var width = this.container.clientWidth;
-    if (isMSIE) {
-        // do IE-specific things
-        width = window.innerWidth;
-    }
-    this.renderer.setSize(window.innerWidth * 0.5, window.innerHeight * 0.5);
-    this.container.appendChild( this.renderer.domElement );
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild( this.renderer.domElement );
     var _this = this;
 
-    this.container.addEventListener('mousedown', function(event) {
-        _this.mouseClicked(event);
-    }, false);
-    this.container.addEventListener('mouseup', function(event) {
-        _this.mouseClicked(event);
-    }, false);
-    this.container.addEventListener('mousemove', function(event) {
-        _this.mouseMoved(event);
+    window.addEventListener('keydown', function(event) {
+        _this.keydown(event);
     }, false);
 
     window.addEventListener('resize', function(event) {
@@ -66,47 +50,40 @@ BaseApp.prototype.createRenderer = function() {
     }, false);
 };
 
-/*
- BaseApp.prototype.keydown = function(event) {
- //Key press functionality
- switch(event.keyCode) {
- case 83: //'S'
- if (this.stats) {
- if (this.statsShowing) {
- $("#Stats-output").hide();
- this.statsShowing = false;
- } else {
- $("#Stats-output").show();
- this.statsShowing = true;
- }
- }
- break;
- case 80: //'P'
- console.log('Cam =', this.camera.position);
- console.log('Look =', this.controls.getLookAt());
- }
- };
- */
+BaseApp.prototype.keydown = function(event) {
+    //Key press functionality
+    switch(event.keyCode) {
+        case 83: //'S'
+            if (this.stats) {
+                if (this.statsShowing) {
+                    $("#Stats-output").hide();
+                    this.statsShowing = false;
+                } else {
+                    $("#Stats-output").show();
+                    this.statsShowing = true;
+                }
+            }
+            break;
+        case 80: //'P'
+            console.log('Cam =', this.camera.position);
+            console.log('Look =', this.controls.getLookAt());
+    }
+};
 
 BaseApp.prototype.mouseClicked = function(event) {
     //Update mouse state
+    this.pickedObjects.length = 0;
+
     if(event.type == 'mouseup') {
         this.mouse.endX = event.clientX;
         this.mouse.endY = event.clientY;
         this.mouse.down = false;
+        this.objectsPicked = false;
         return;
     }
-    this.mouse.startX = event.clientX;
-    this.mouse.startY = event.clientY;
+    this.mouse.startX = (event.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.startY = (event.clientY / window.innerHeight) * 2 + 1;
     this.mouse.down = true;
-
-    var vector = new THREE.Vector3(( event.clientX / window.innerWidth ) * 2 - 1, -( event.clientY / window.innerHeight ) * 2 + 1, 0.5);
-    this.projector.unprojectVector(vector, this.camera);
-
-    var raycaster = new THREE.Raycaster(this.camera.position, vector.sub(this.camera.position).normalize());
-
-    this.pickedObjects.length = 0;
-    this.pickedObjects = raycaster.intersectObjects(this.scene.children, true);
 };
 
 BaseApp.prototype.mouseMoved = function(event) {
@@ -117,11 +94,10 @@ BaseApp.prototype.mouseMoved = function(event) {
 
 BaseApp.prototype.windowResize = function(event) {
     //Handle window resize
-    this.camera.aspect = 640 / 480;
+    this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
 
-    //this.renderer.setSize( window.innerWidth * 0.5, window.innerHeight * 0.5);
-    this.renderer.setSize(640, 480);
+    this.renderer.setSize( window.innerWidth, window.innerHeight);
     //console.log('Size =', )
 };
 
@@ -132,32 +108,31 @@ BaseApp.prototype.createScene = function() {
     this.scene.add(ambientLight);
 
     /*
-     var spotLight = new THREE.SpotLight(0xffffff);
-     spotLight.position.set(100, 100, 200);
-     spotLight.intensity = 1;
-     this.scene.add(spotLight);
-     */
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(100, 100, 200);
+    spotLight.intensity = 1;
+    this.scene.add(spotLight);
+    */
 
     /*
-     var directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
-     directionalLight.position.set( 1, 1, 1 );
-     this.scene.add( directionalLight );
-     */
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
+    directionalLight.position.set( 1, 1, 1 );
+    this.scene.add( directionalLight );
+    */
 
 
-    var pointLight = new THREE.PointLight(0xffffff);
-    pointLight.castShadow = true;
-    pointLight.position.set(50,0,0);
-    pointLight.castShadow = true;
-    pointLight.name = 'PointLight';
-    this.scene.add(pointLight);
+    this.pointLight = new THREE.PointLight(0xffffff);
+    this.pointLight.castShadow = true;
+    this.pointLight.position.set(50,0,0);
+    this.pointLight.name = 'PointLight';
+    this.scene.add(this.pointLight);
 
 };
 
 BaseApp.prototype.createCamera = function() {
 
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 5000 );
-    this.camera.position.set(0, 0, 50 );
+    this.camera.position.set(0, 0, 2 );
 
     console.log('dom =', this.renderer.domElement);
 };
@@ -168,16 +143,18 @@ BaseApp.prototype.createControls = function() {
     this.controls.zoomSpeed = 1.0;
     this.controls.panSpeed = 1.0;
 
-    this.controls.noZoom = false;
-    this.controls.noPan = false;
-
     this.controls.staticMoving = true;
     this.controls.dynamicDampingFactor = 0.3;
 
     this.controls.keys = [ 65, 83, 68 ];
 
-    var lookAt = new THREE.Vector3(0, 10, 0);
+    var lookAt = new THREE.Vector3(0, 0, 0);
     this.controls.setLookAt(lookAt);
+};
+
+BaseApp.prototype.setCamera = function(cameraProp) {
+    this.camera.position.set(cameraProp[0].x, cameraProp[0].y, cameraProp[0].z);
+    this.controls.setLookAt(cameraProp[1]);
 };
 
 BaseApp.prototype.update = function() {
@@ -186,12 +163,12 @@ BaseApp.prototype.update = function() {
 };
 
 BaseApp.prototype.run = function() {
-    var self = this;
+    this.renderer.render( this.scene, this.camera );
+    var _this = this;
     this.update();
-    //this.renderer.render( this.scene, this.camera );
     if(this.stats) this.stats.update();
     requestAnimationFrame(function() {
-        self.run();
+        _this.run();
     });
 };
 
--- js/parm.js
+++ js/parm.js
@@ -31,6 +31,14 @@ Parm.prototype.init = function(container) {
     
     this.renderObjects = [];
     this.markerObjects = [];
+
+    this.jsArucoMarker	= new THREEx.JsArucoMarker();
+    this.jsArucoMarker.debugEnabled = this.markerDebugEnabled;
+    location.hash = '#webcam';
+    this.videoGrabbing = new THREEx.WebcamGrabbing();
+    this.jsArucoMarker.videoScaleDown = 2;
+    document.body.appendChild(this.videoGrabbing.domElement);
+    this.markerIndex = undefined;
     
     return true;
 };
@@ -42,28 +50,44 @@ Parm.prototype.createScene = function() {
     var manager = new THREE.LoadingManager();
     this.loader = new THREE.OBJLoader( manager );
 
-    var models = ["", "building_one_reduced_x2.obj", "pottery.obj"];
+    var models = [
+        {name: "", scale: 1.0},
+        {name: "building_one_reduced_x2.obj", scale: 0.01},
+        {name: "pottery.obj", scale: 0.1}
+    ];
+
     this.objectIDs = [1, 30, 265];
-    for(var i=0; i<this.objectIDs.length; ++i) {
+    this.numObjects = this.objectIDs.length;
+    for(var i=0; i<this.numObjects; ++i) {
         this.markerObjects.push(new THREE.Object3D());
         this.markerObjects[i].markerID = this.objectIDs[i];
-        scene.add(this.markerObjects[i]);
+        this.scene.add(this.markerObjects[i]);
+        this.markerObjects[i].visible = false;
         this.setupMarkerObject(this.markerObjects[i], models[i]);
     }
 };
 
 Parm.prototype.setupMarkerObject = function(markerObject, model) {
     //Associate model with marker
-    if(model !== "") {
-        this.loader.load("models/"+model, function(object) {
-            object.scale.set(0.1, 0.1, 0.1);
+    if(model.name !== "") {
+        this.loader.load("models/"+model.name, function(object) {
+            object.scale.set(model.scale, model.scale, model.scale);
+            object.rotation.x = Math.PI/2;
             markerObject.add(object);
         })
     } else {
-        var geometry = new THREE.BoxGeometry(1,1,1);
+        var planeGeometry = new THREE.PlaneGeometry(5,5);
+        var planeMaterial =    new THREE.MeshLambertMaterial({color: 0xffffff});
+        var plane = new THREE.Mesh(planeGeometry,planeMaterial);
+        plane.receiveShadow  = true;
+        plane.position.set(0, 0, -0.75);
+        markerObject.add(plane);
+
+        var geometry = new THREE.SphereGeometry(0.5,16,16);
         var boxMat = new THREE.MeshLambertMaterial({ color: 0x00ff00});
-        var box = new THREE.Mesh(geometry, boxMat);
-        markerObject.add(box);
+        var sphere = new THREE.Mesh(geometry, boxMat);
+        sphere.castShadow = true;
+        markerObject.add(sphere);
     }
 };
 
@@ -84,9 +108,28 @@ Parm.prototype.createGUI = function() {
 Parm.prototype.update = function() {
     //Perform any updates
     //this.delta = this.clock.getDelta();
-
+    var _this = this, markerIndex = undefined;
     BaseApp.prototype.update.call(this);
 
+    var domElement	= this.videoGrabbing.domElement;
+    var markers	= this.jsArucoMarker.detectMarkers(domElement);
+    for(var i=0; i<this.numObjects; ++i) {
+        this.markerObjects[i].visible = false;
+    }
+    markers.forEach(function(marker){
+        // if( marker.id !== 265 )	return
+        for(i=0; i<_this.numObjects; ++i) {
+            if(marker.id === _this.markerObjects[i].markerID) {
+                markerIndex = i;
+                break;
+            }
+        }
+        if(markerIndex === undefined) return;
+
+        _this.jsArucoMarker.markerToObject3D(marker, _this.markerObjects[markerIndex]);
+
+        _this.markerObjects[markerIndex].visible = true;
+    })
 };
 
 $(document).ready(function() {
@@ -94,9 +137,8 @@ $(document).ready(function() {
     if(!Detector.webgl) {
         $('#notSupported').show();
     } else {
-        var container = document.getElementById("WebGL-output");
         var app = new Parm();
-        if(!app.init(container)) {
+        if(!app.init(null)) {
             //DEBUG
             console.log("Media not supported");
             return;
